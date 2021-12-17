76b8d0c1068f2f8369e5723f50ad7eb8edcbfc8c
viper23 <htonnycarlos@gmail.com>
2021-12-17 18:43:03 +0200

    added function that display error

--- index.js
+++ index.js
@@ -20,11 +20,26 @@ const agentConfirmChecker = (value) =>{
 
 };
 
+// function that will display errors while entering 
+const errorDisplay = (tag, message, valid) =>{
+    const container = document.querySelector("."+tag+"-container");
+    const span = document.querySelector("."+tag+"-container > span");
+    //this if clause shows us if
+    if (!valid) {
+        container.classList.add('error');
+        span.textContent = message;
+    }else{
+        container.classList.remove('error');
+        span.textContent = message;        
+    }
+}; 
+
 // shows us which input we are typing on /*console.log(e.target.id);*/
 inputs.forEach((input) =>{
     input.addEventListener("input", (e)=>{
         switch (e.target.id) {
             case "pseudo":
+                
                 agentChecker(e.target.value);
                 break;
             case "email":
